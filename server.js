0c41f3f8085f9b9837155f6d96cb4c02a68e8a0d
Simon Johansson <mail@simon-johansson.com>
2018-12-01 22:34:06 +0100

    Fix sendFile bug and remove unused code

--- server.js
+++ server.js
@@ -2,25 +2,10 @@ var express = require("express");
 var path = require("path");
 var app = express();
 
-function sslRedirect(environments) {
-  environments = environments || ["production"];
-  return function(req, res, next) {
-    if (environments.indexOf(process.env.NODE_ENV) >= 0) {
-      if (req.headers["x-forwarded-proto"] != "https") {
-        res.redirect(301, "https://" + req.hostname + req.originalUrl);
-      } else next();
-    } else next();
-  };
-}
-
 app.set("port", process.env.PORT || 8080);
-
-// app.use(sslRedirect());
-
 app.use(express.static(path.join(__dirname, "dist")));
-
 app.use("*", function(req, res) {
-  res.sendFile(__dirname, "dist", "index.html");
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 var server = app.listen(app.get("port"), function() {
