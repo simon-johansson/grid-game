5e55aebff0ee7b3c29ca206560c5bf9992267793
Simon Johansson <mail@simon-johansson.com>
2018-12-01 22:48:53 +0100

    Fixing server... hopefully

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var express = require("express");
 var path = require("path");
 var app = express();
+var port = process.env.PORT || 8080;
 
-app.set("port", process.env.PORT || 8080);
+app.set("port", port);
 app.use(express.static(path.join(__dirname, "dist")));
-app.use("*", function(req, res) {
+app.use("/", function(req, res) {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-var server = app.listen(app.get("port"), function() {
-  var port = server.address().port;
+app.listen(port, function() {
   console.log("Server running on port", port);
 });
