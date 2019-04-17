37ac881812b01c02dfe80007541a43726de5fc5a
Simon Johansson <mail@simon-johansson.com>
2019-04-17 22:41:05 +0200

    Create new Sentry releases from Heroku

---
+++ build_scripts/sentry.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+REVISION="${SOURCE_VERSION}"
+# PROPOSED_VERSION=$(npx sentry-cli releases propose-version)
+APP_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",\t ]//g')
+VERSION="$APP_VERSION-$REVISION"
+
+if [ "$NODE_ENV" = "production" ]
+then
+  npx sentry-cli releases new $VERSION
+  npx sentry-cli releases files $VERSION upload-sourcemaps dist --no-rewrite --validate
+  # npx sentry-cli releases set-commits $VERSION --auto
+  sentry-cli releases set-commits --commit "simon-johansson/grid-game@${REVISION}" $VERSION
+  npx sentry-cli releases finalize $VERSION
+else
+  echo "Not creating new Sentry release because \$NODE_ENV is set to $NODE_ENV and not 'production'";
+fi
